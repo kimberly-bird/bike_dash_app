877f1a622f2f22084d37801c6280503671b06dd6
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-01-26 20:08:11 -0600

    quick sql script with a few insert statements to seed db

---
+++ bikes/bike_script.sql
@@ -0,0 +1,14 @@
+INSERT INTO bikes_condition VALUES (null, 'New');
+INSERT INTO bikes_condition VALUES (null, 'Good');
+INSERT INTO bikes_condition VALUES (null, 'Fair');
+
+INSERT INTO bikes_status VALUES (null, 'Sold');
+INSERT INTO bikes_status VALUES (null, 'Listed');
+
+INSERT INTO bikes_brand VALUES (null, 'Surley', 'Minneapolis');
+INSERT INTO bikes_brand VALUES (null, 'Salsa', 'Minneapolis');
+
+INSERT INTO bikes_bikemodel VALUES (null, 'Big Fat Dummy', 1);
+INSERT INTO bikes_bikemodel VALUES (null, 'Bridge Club', 1);
+INSERT INTO bikes_bikemodel VALUES (null, 'War Bird', 2);
+
