091e49b5f6d4a6d594b165896b4a00281715eea7
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-01-30 13:03:34 -0600

    saved message appears when creating new bike brand

--- bikes/views/brand_add_view.py
+++ bikes/views/brand_add_view.py
@@ -1,6 +1,9 @@
 from bikes.forms import UserForm, BrandForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from bikes.models import Brand
 
@@ -25,5 +28,6 @@ def add_brand(request):
             location = form_data['location'],
         )
         newBrand.save()
+        messages.success(request, 'Saved!')
+        return HttpResponseRedirect(reverse("bikes:brand_list"))
 
-        return render(request, 'brands/success.html', {})
