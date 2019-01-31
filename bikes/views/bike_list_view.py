752c526a07aec651aee0210dc8d61b660932e2a7
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-01-31 11:39:28 -0600

    lists bikes based on logged in user

--- bikes/views/bike_list_view.py
+++ bikes/views/bike_list_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from bikes.models import Bike
+from django.contrib.auth.models import User
 
 
 def bike_list(request):
@@ -11,6 +12,8 @@ def bike_list(request):
         returns rendered list of all bikes 
     '''
     if request.method == "GET":
-        bikes = Bike.objects.all()
+        current_user = request.user
+        print(current_user.id)
+        bikes = Bike.objects.filter(user_id=current_user.id)
         context = {"bike_list": bikes}
         return render(request, 'bikes/list.html', context)
