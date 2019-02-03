980393ac31c531ece566a934695a562b95abe71b
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-02-02 21:38:14 -0600

    updated sql script to include nulls for safe delete

--- bikes/bike_script.sql
+++ bikes/bike_script.sql
@@ -1,20 +1,20 @@
-INSERT INTO bikes_condition VALUES (null, 'New');
-INSERT INTO bikes_condition VALUES (null, 'Good');
-INSERT INTO bikes_condition VALUES (null, 'Fair');
-INSERT INTO bikes_condition VALUES (null, 'Poor');
+INSERT INTO bikes_condition VALUES (null, null, 'New');
+INSERT INTO bikes_condition VALUES (null, null, 'Good');
+INSERT INTO bikes_condition VALUES (null, null, 'Fair');
+INSERT INTO bikes_condition VALUES (null, null, 'Poor');
 
-INSERT INTO bikes_status VALUES (null, 'Sold');
-INSERT INTO bikes_status VALUES (null, 'Listed');
-INSERT INTO bikes_status VALUES (null, 'In Process');
+INSERT INTO bikes_status VALUES (null, null, 'Sold');
+INSERT INTO bikes_status VALUES (null, null, 'Listed');
+INSERT INTO bikes_status VALUES (null, null, 'In Process');
 
-INSERT INTO bikes_brand VALUES (null, 'Surly', 'Minneapolis');
-INSERT INTO bikes_brand VALUES (null, 'Salsa', 'Minneapolis');
+INSERT INTO bikes_brand VALUES (null, null, 'Surly', 'Minneapolis');
+INSERT INTO bikes_brand VALUES (null, null, 'Salsa', 'Minneapolis');
 
-INSERT INTO bikes_bikemodel VALUES (null, 'Big Fat Dummy', 1);
-INSERT INTO bikes_bikemodel VALUES (null, 'Bridge Club', 1);
-INSERT INTO bikes_bikemodel VALUES (null, 'War Bird', 2);
+INSERT INTO bikes_bikemodel VALUES (null, null, 'Big Fat Dummy', 1);
+INSERT INTO bikes_bikemodel VALUES (null, null, 'Bridge Club', 1);
+INSERT INTO bikes_bikemodel VALUES (null, null, 'War Bird', 2);
 
-INSERT INTO bikes_bike VALUES (null, '2019-01-30', 'BFD Surley', '2018', 'very cool bike', 500, '2019-01-22', null, null, null, 1, 1, 2, 3, 1);
-INSERT INTO bikes_bike VALUES (null, '2019-01-30', 'War Bird', '2018', 'very fast bike', 300, '2019-01-10', null, null, null, 2, 3, 2, 3, 1);
+INSERT INTO bikes_bike VALUES (null, null, '2019-01-30', 'BFD Surley', '2018', 'very cool bike', 500, '2019-01-22', null, null, null, 1, 1, 2, 3, 1);
+INSERT INTO bikes_bike VALUES (null, null, '2019-01-30', 'War Bird', '2018', 'very fast bike', 300, '2019-01-10', null, null, null, 3, 2, 2, 3, 1);
 
 
