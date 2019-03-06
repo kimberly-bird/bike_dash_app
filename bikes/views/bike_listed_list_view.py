603c858bb16a12c5c34b61316e868b62b706e190
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-03-06 11:10:02 -0600

    updated calculating bike totals on sub nav

--- bikes/views/bike_listed_list_view.py
+++ bikes/views/bike_listed_list_view.py
@@ -14,7 +14,12 @@ def bike_listed_list(request):
     
     if request.method == "GET":
         current_user = request.user
-        bikes = Bike.objects.order_by('-status_id').filter(user_id=current_user.id, status_id=2)
-        context = {"bike_list": bikes}
+        bikes = Bike.objects.order_by('-status_id').filter(user_id=current_user.id)
+        bikes_listed = Bike.objects.filter(user_id=current_user.id, status_id=2)
+        all_bike_count = bikes.count()
+        sold_bikes = bikes.filter(status_id=1).count()
+        listed_bikes = bikes.filter(status_id=2).count()
+        in_process_bikes = bikes.filter(status_id=3).count()
+        context = {"bike_list": bikes_listed, "all_bike_count": all_bike_count, "sold_bikes": sold_bikes, "listed_bikes": listed_bikes, "in_process_bikes": in_process_bikes}
         return render(request, 'bikes/listed_list.html', context)
         
