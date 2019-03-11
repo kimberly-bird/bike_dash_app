7d2e170f1c711eff6a555b24ee57cb00e4d50843
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-03-10 20:11:17 -0500

    code is now more dry, phew

--- bikes/views/bike_list_view.py
+++ bikes/views/bike_list_view.py
@@ -4,7 +4,7 @@ from bikes.models import Bike
 from django.contrib.auth.models import User
 
 
-def bike_list(request):
+def bike_list(request, status_id):
     '''View for list of user's bikes
 
         Allowed verbs: GET
@@ -14,11 +14,32 @@ def bike_list(request):
     
     if request.method == "GET":
         current_user = request.user
-        bikes = Bike.objects.order_by('name').filter(user_id=current_user.id)
-        all_bike_count = bikes.count()
-        sold_bikes = bikes.filter(status_id=1).count()
-        listed_bikes = bikes.filter(status_id=2).count()
-        in_process_bikes = bikes.filter(status_id=3).count()
-        context = {"bike_list": bikes, "all_bike_count": all_bike_count, "sold_bikes": sold_bikes, "listed_bikes": listed_bikes, "in_process_bikes": in_process_bikes}
+
+        # all bikes
+        all_bikes = Bike.objects.order_by('-status').filter(user_id=current_user.id)
+        all_bike_count = all_bikes.count()
+
+        # bikes by in process status
+        bikes_in_process = all_bikes.filter(user_id=current_user.id, status_id=3)
+        in_process_bikes = bikes_in_process.count()
+
+        # bikes by listed status
+        bikes_listed = all_bikes.filter(user_id=current_user.id, status_id=2)
+        listed_bikes_count = bikes_listed.count()
+
+        # bikes by sold status
+        bikes_sold = all_bikes.filter(user_id=current_user.id, status_id=1)
+        sold_bikes_count = bikes_sold.count()
+
+        context = {
+            "bike_list": all_bikes, 
+            "all_bike_count": all_bike_count, 
+            "bikes_in_process": bikes_in_process,
+            "in_process_bikes_count": in_process_bikes, 
+            "bikes_listed": bikes_listed,
+            "listed_bikes_count": listed_bikes_count, 
+            "sold_bikes_count": sold_bikes_count, 
+            "bikes_sold": bikes_sold,
+            }
         return render(request, 'bikes/list.html', context)
         
