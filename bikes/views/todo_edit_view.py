32fa9274e7edc96d2b15d52c94f424995feb1565
Kimberly Bird <kimberly.j.bird@gmail.com>
2019-03-17 20:42:05 -0500

    fixed edit todo bug

--- bikes/views/todo_edit_view.py
+++ bikes/views/todo_edit_view.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
@@ -20,10 +21,15 @@ def edit_todo(request, pk):
     todo = get_object_or_404(ToDo, pk=pk)
 
     if request.method == "POST":
-        todo_form = ToDoForm(request.POST, instance=todo)
-        if todo_form.is_valid():
-            todo = todo_form.save()
-            return HttpResponseRedirect(reverse('bikes:bike_list', args=(4,)))
+        current_user = request.user
+        todo.user = current_user
+        todo.bike = Bike.objects.get(id=request.POST['bike'])
+        todo.notes = request.POST['notes']
+        todo.date = request.POST['date']
+        todo.title = request.POST['title']
+        todo.save()
+        messages.success(request, "Edited!")
+        return HttpResponseRedirect(reverse('bikes:todo_list'))
     else:
         todo_form = ToDoForm(instance=todo, request=request)
 
